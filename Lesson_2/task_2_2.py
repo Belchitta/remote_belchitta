cb19fbd1de15766fe063ce4bf8128434ec9d45f8
Belchitta <misschernenok@gmail.com>
2022-04-25 17:50:45 +0300

    My dz_

---
+++ Lesson_2/task_2_2.py
@@ -0,0 +1,13 @@
+some_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+digits = []
+# for symbol in some_list:
+#     if '517'.find(symbol) != -1:
+#         digits.append(int(symbol))
+# print(digits)
+some_list.pop(1)
+some_list.pop(-2)
+some_list.insert(1, '05')
+some_list.insert(-1, '+05')
+print(some_list)
+format(some_list, __format_spec= )
+print(some_list)
