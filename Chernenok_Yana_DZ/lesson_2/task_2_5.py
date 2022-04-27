a2490a0a928d3a5274b87f90816ef653e52f17cb
Belchitta <misschernenok@gmail.com>
2022-04-27 12:10:16 +0300

    dz_lesson_2

--- Chernenok_Yana_DZ/lesson_2/task_2_5.py
+++ Chernenok_Yana_DZ/lesson_2/task_2_5.py
@@ -2,7 +2,6 @@ price_list = [5, 10.7, 20.90, 21.05, 17.00, 15.99, 30.05, 31.99, 80.50, 50.50]
 # A
 for price in price_list:
     print(f"{int(price)} руб. {int(price * 100 % 100):02} коп.", end=', ')
-
 # B
 print('\nid price_list', id(price_list))
 price_list.sort()
